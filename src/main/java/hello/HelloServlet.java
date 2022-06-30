2a2e0a090acd41c54cb3795cfe3e16c6aa7e4bbd
Danilo Murilo <daruloto128@gmail.com>
2022-06-29 22:22:54 -0300

    Atualizando classes java

--- src/main/java/hello/HelloServlet.java
+++ src/main/java/hello/HelloServlet.java
@@ -20,6 +20,7 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author Danilo
  */
+@SuppressWarnings("serial")
 @WebServlet("/alomundo")
 public class HelloServlet extends HttpServlet {
 	
@@ -46,11 +47,41 @@ public class HelloServlet extends HttpServlet {
         try (PrintWriter out = response.getWriter()) {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
+            out.println("<link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC\" crossorigin=\"anonymous\">");
             out.println("<head>");
-            out.println("<title>Servlet HelloServlet</title>");            
+            out.println("<title>Servlet HelloServlet</title>");
+            out.println("<nav class=\"navbar navbar-expand-lg navbar-light bg-light\">\r\n"
+            		+ "	  <div class=\"container-fluid\">\r\n"
+            		+ "	    <a class=\"navbar-brand\" href=\"https://github.com/daruloto128/helloheroku/\">Git Hub</a>\r\n"
+            		+ "	    <button class=\"navbar-toggler\" type=\"button\" data-bs-toggle=\"collapse\" data-bs-target=\"#navbarSupportedContent\" aria-controls=\"navbarSupportedContent\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">\r\n"
+            		+ "	      <span class=\"navbar-toggler-icon\"></span>\r\n"
+            		+ "	    </button>\r\n"
+            		+ "	    <div class=\"collapse navbar-collapse\" id=\"navbarSupportedContent\">\r\n"
+            		+ "	      <ul class=\"navbar-nav me-auto mb-2 mb-lg-0\">\r\n"
+            		+ "	        <li class=\"nav-item\">\r\n"
+            		+ "	          <a class=\"nav-link active\" aria-current=\"page\" href=\"https://hwhdanilouff.herokuapp.com/\">Home</a>\r\n"
+            		+ "	        </li>\r\n"
+            		+ "	        <li class=\"nav-item\">\r\n"
+            		+ "	          <a class=\"nav-link\" href=\".\\alomundo?lang=pt&nome=Fulano\">Método GET</a>\r\n"
+            		+ "	        </li>\r\n"
+            		+ "	        <li class=\"nav-item\">\r\n"
+            		+ "	          <a class=\"nav-link\" href=\".\\helloform.html\">Método POST</a>\r\n"
+            		+ "	        </li>	        \r\n"
+            		+ "	        <li class=\"nav-item\">\r\n"
+            		+ "	          <a class=\"nav-link disabled\" href=\"#\" tabindex=\"-1\" aria-disabled=\"true\">Hello World - em outros idiomas</a>\r\n"
+            		+ "	        </li>\r\n"
+            		+ "	      </ul>\r\n"
+            		+ "	      <form class=\"d-flex\">\r\n"
+            		+ "	        <input class=\"form-control me-2\" type=\"search\" placeholder=\"Search\" aria-label=\"Search\">\r\n"
+            		+ "	        <button class=\"btn btn-outline-success\" type=\"submit\">Buscar</button>\r\n"
+            		+ "	      </form>\r\n"
+            		+ "	    	</div>\r\n"
+            		+ "	  	</div>\r\n"
+            		+ "	</nav>\r\n"
+            		+ "	<br><br><br>");
             out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>" +ola+nome+ "!</h1>"+"Horario: "+calendar.get((Calendar.HOUR_OF_DAY)));            
+            out.println("<body>");         
+            out.println("<div class=\"container\"> <div class=\"row\"> <h1> <div class=\"col-md-9 ms-md-auto\">" +ola+nome+ "!</h1></div>"+"<div class=\"col-md-9 ms-md-auto\">Horario:"+calendar.get((Calendar.HOUR_OF_DAY))+"H </div> </div></div>");            
             out.println("</body>");
             out.println("</html>");
         }
@@ -75,13 +106,44 @@ public class HelloServlet extends HttpServlet {
 
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
+            out.println("<link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC\" crossorigin=\"anonymous\">");
             out.println("<head>");
             out.println("<title>Servlet HelloServlet</title>");            
+            out.println("<nav class=\"navbar navbar-expand-lg navbar-light bg-light\">\r\n"
+            		+ "	  <div class=\"container-fluid\">\r\n"
+            		+ "	    <a class=\"navbar-brand\" href=\"https://github.com/daruloto128/helloheroku/\">Git Hub</a>\r\n"
+            		+ "	    <button class=\"navbar-toggler\" type=\"button\" data-bs-toggle=\"collapse\" data-bs-target=\"#navbarSupportedContent\" aria-controls=\"navbarSupportedContent\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">\r\n"
+            		+ "	      <span class=\"navbar-toggler-icon\"></span>\r\n"
+            		+ "	    </button>\r\n"
+            		+ "	    <div class=\"collapse navbar-collapse\" id=\"navbarSupportedContent\">\r\n"
+            		+ "	      <ul class=\"navbar-nav me-auto mb-2 mb-lg-0\">\r\n"
+            		+ "	        <li class=\"nav-item\">\r\n"
+            		+ "	          <a class=\"nav-link active\" aria-current=\"page\" href=\"https://hwhdanilouff.herokuapp.com/\">Home</a>\r\n"
+            		+ "	        </li>\r\n"
+            		+ "	        <li class=\"nav-item\">\r\n"
+            		+ "	          <a class=\"nav-link\" href=\".\\alomundo?lang=pt&nome=Fulano\">Método GET</a>\r\n"
+            		+ "	        </li>\r\n"
+            		+ "	        <li class=\"nav-item\">\r\n"
+            		+ "	          <a class=\"nav-link\" href=\".\\helloform.html\">Método POST</a>\r\n"
+            		+ "	        </li>	        \r\n"
+            		+ "	        <li class=\"nav-item\">\r\n"
+            		+ "	          <a class=\"nav-link disabled\" href=\"#\" tabindex=\"-1\" aria-disabled=\"true\">Hello World - em outros idiomas</a>\r\n"
+            		+ "	        </li>\r\n"
+            		+ "	      </ul>\r\n"
+            		+ "	      <form class=\"d-flex\">\r\n"
+            		+ "	        <input class=\"form-control me-2\" type=\"search\" placeholder=\"Search\" aria-label=\"Search\">\r\n"
+            		+ "	        <button class=\"btn btn-outline-success\" type=\"submit\">Buscar</button>\r\n"
+            		+ "	      </form>\r\n"
+            		+ "	    	</div>\r\n"
+            		+ "	  	</div>\r\n"
+            		+ "	</nav>\r\n"
+            		+ "	<br><br><br>");
             out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>" +ola+nome+"!</h1>"+"Horario: "+calendar.get((Calendar.HOUR_OF_DAY)));            
+            out.println("<body>");         
+            out.println("<div class=\"container\"> <div class=\"row\"> <h1> <div class=\"col-md-9 ms-md-auto\">" +ola+nome+ "!</h1></div>"+"<div class=\"col-md-9 ms-md-auto\">Horario:"+calendar.get((Calendar.HOUR_OF_DAY))+"H </div> </div></div>");            
             out.println("</body>");
             out.println("</html>");
         }
@@ -113,7 +175,7 @@ public class HelloServlet extends HttpServlet {
         	if(calendar.get(Calendar.HOUR_OF_DAY) <= 12) {
         		msg = "Bonjour, ";
         	}else if(calendar.get((Calendar.HOUR_OF_DAY)) > 12 && (calendar.get(Calendar.HOUR_OF_DAY) <= 18)) {
-        		msg = "Bonne aprï¿½s-midi, ";
+        		msg = "Bon après-midi, ";
         	}else if(calendar.get(Calendar.HOUR_OF_DAY) > 18){
         		msg = "Bonne nuit, ";
         	}
